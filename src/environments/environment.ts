9a5084b3c95713ef89cbc57bfe897a2a9b40497b
ibrahim <ibrahimcherri@outlook.com>
2022-12-12 10:08:15 +0100

    scratch v0

---
+++ src/environments/environment.ts
@@ -0,0 +1,14 @@
+
+export const environment = {
+  firebase: {
+    projectId: 'systeme-domotique',
+    appId: '1:8230798147:web:4bd448c6586cba4f6065f6',
+    databaseURL: 'https://systeme-domotique-default-rtdb.europe-west1.firebasedatabase.app',
+    storageBucket: 'systeme-domotique.appspot.com',
+    apiKey: 'AIzaSyDj3ZRsuLaibBHgBECUafQBZ_4jPnWQE0w',
+    authDomain: 'systeme-domotique.firebaseapp.com',
+    messagingSenderId: '8230798147',
+    measurementId: 'G-V7Y44JX4JN',
+  },
+    production: false
+};
